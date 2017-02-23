dab40ec1994a6e7f9fd5472e0b5e0084350247c9
Marina Rusakova <marinarusakova111@gmail.com>
2017-02-23 03:50:23 -0800

    Выполнила задание c модулем загрузки информации 3

--- js/load.js
+++ js/load.js
@@ -7,26 +7,26 @@ window.load = (function () {
 
     xhr.open('GET', url);
 
-    xhr.addEventListener ('load', function(evt) {
+    xhr.addEventListener('load', function (evt) {
       try {
         if (evt.target.status >= 400) {
           /* Евгений, кoд ниже закоментирован, потому что не проходит проверку Travis */
-          /*console.log('Failed to load data. Server returned status: ' + evt.target.status);*/
+          /* console.log('Failed to load data. Server returned status: ' + evt.target.status);*/
         } else if (evt.target.status >= 200) {
           var result = JSON.parse(evt.target.response);
           onLoad(result);
         }
-      } catch(err){/*console.log(err)*/}
+      } catch(err) { /* console.log(err)*/ }
     });
 
-    xhr.addEventListener ('error', function(e) {
+    xhr.addEventListener('error', function (e) {
       /* Евгений, кoд ниже закоментирован, потому что не проходит проверку Travis */
-      /*console.log('Something\'s went wrong!');*/
+      /* console.log('Something\'s went wrong!'); */
     });
 
     xhr.addEventListener('timeout', function() {
       /* Евгений, кoд ниже закоментирован, потому что не проходит проверку Travis */
-      /*console.log('Time\'s up!');*/
+      /* console.log('Time\'s up!'); */
     });
 
     xhr.send();
