684d5a681da8c4850a862905200fffd208ce0222
Marina Rusakova <marinarusakova111@gmail.com>
2017-02-23 03:20:40 -0800

    Выполнила задание c модулем загрузки информации

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -3,10 +3,15 @@
 window.initializePins = (function () {
   var ENTER_KEY_CODE = 13;
 
+  var RENDERED_OFFERS_COUNT = 3;
+
   var offerDetailsDialog = document.querySelector('.dialog');
-  var availableOffers = document.querySelectorAll('.tokyo__pin-map .pin');
+  var availableOffers = [];
   var offerDetailsDialogCloseBtn = document.querySelector('.dialog__close');
   var offerMap = document.querySelector('.tokyo__pin-map');
+
+  var similarOffersUrl = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
+  var similarApartments = [];
 
   var isEnterPressed = function (evt) {
     return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
@@ -23,7 +28,9 @@ window.initializePins = (function () {
   };
 
   var clickHandler = function (evt) {
-    window.card.showCard(evt.target.parentElement, offerDetailsDialog, function () {
+    var offerDataIndex = evt.target.parentElement.getAttribute('offer-id');
+
+    window.card.showCard(similarApartments[offerDataIndex], evt.target.parentElement, offerDetailsDialog, function () {
       clearOfferSelections();
       highlightOffer(evt.target.parentElement);
     }, function () {
@@ -33,7 +40,9 @@ window.initializePins = (function () {
 
   var offerKeypressHandler = function (evt) {
     if (isEnterPressed(evt)) {
-      window.card.showCard(evt.target, offerDetailsDialog, function () {
+      var offerDataIndex = evt.target.getAttribute('offer-id');
+
+      window.card.showCard(similarApartments[offerDataIndex], evt.target, offerDetailsDialog, function () {
         clearOfferSelections();
         highlightOffer(evt.target);
       }, function () {
@@ -43,13 +52,40 @@ window.initializePins = (function () {
     }
   };
 
+  var renderNewAvailableOffers = function () {
+    var renderedOffers = similarApartments.slice(0, RENDERED_OFFERS_COUNT);
+
+    var template = document.querySelector('#pin-template');
+    var offerTemplateToClone = template.content.querySelector('.pin');
+
+    for (var i = 0; i < renderedOffers.length; i++) {
+      var newOffer = offerTemplateToClone.cloneNode(true);
+
+      newOffer.style.left = renderedOffers[i].location.x + 'px';
+      newOffer.style.top = renderedOffers[i].location.y + 'px';
+      
+      newOffer.getElementsByTagName("img")[0].src = renderedOffers[i].author.avatar;
+      newOffer.getElementsByTagName("img")[0].alt = renderedOffers[i].offer.address;
+
+      newOffer.setAttribute('offer-id', i);
+
+      newOffer.addEventListener('keypress', offerKeypressHandler);
+
+      availableOffers.push(newOffer); 
+
+      offerMap.appendChild(newOffer);
+    }
+
+  };
+
   return function () {
 
-    offerMap.addEventListener('click', clickHandler, true);
-
-    availableOffers.forEach(function (offer) {
-      offer.addEventListener('keypress', offerKeypressHandler);
+    window.load(similarOffersUrl, function (similarOffers) {
+      similarApartments = similarOffers;
+      renderNewAvailableOffers();
     });
+
+    offerMap.addEventListener('click', clickHandler, true);
 
     offerDetailsDialogCloseBtn.addEventListener('click', function () {
       window.card.hideCard(offerDetailsDialog);
--- js/show-card.js
+++ js/show-card.js
@@ -3,9 +3,53 @@
 window.card = (function () {
 
   var onCardCloseHandler = null;
+  var offerDialogPanel = document.querySelector('.dialog__panel');
+
+  var dialogPanelTemplate = document.querySelector('#dialog-panel-template');
+
+  var dialogFeatureTemplate = document.querySelector('#features-panel-template');
+
+  var dialogPhotosTemplate = document.querySelector('#photos-panel-template');
 
   return {
-    showCard: function (selectedOffer, offerDetailsDialog, onCardOpen, onCardClose) {
+    showCard: function (data, selectedOffer, offerDetailsDialog, onCardOpen, onCardClose) {
+
+      document.querySelector('.dialog__title img').src = data.author.avatar;
+
+      var newDialog = dialogPanelTemplate.content.cloneNode(true);
+
+      newDialog.querySelector('.lodge__title').textContent = data.offer.title;
+      newDialog.querySelector('.lodge__address').textContent = data.offer.address;
+      newDialog.querySelector('.lodge__price').textContent = data.offer.price + ' ₽/ночь';
+      newDialog.querySelector('.lodge__type').textContent = data.offer.type;
+      newDialog.querySelector('.lodge__rooms-and-guests').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+      newDialog.querySelector('.lodge__checkin-time').textContent = 'Заед после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+      newDialog.querySelector('.lodge__description').textContent = data.offer.description;
+
+      var featureList = newDialog.querySelector('.lodge__features');
+
+      featureList.innerHTML = "";
+
+      data.offer.features.forEach(function (feature) {
+        var newFeature = dialogFeatureTemplate.content.querySelector('.feature__image').cloneNode(true);
+        newFeature.classList.add('feature__image--' + feature);
+        featureList.appendChild(newFeature);
+      });
+
+      var photosList = newDialog.querySelector('.lodge__photos');
+
+      photosList.innerHTML = "";
+
+      data.offer.photos.forEach(function (photo) {
+        var newPhoto = dialogPhotosTemplate.content.querySelector('img').cloneNode(true);
+        newPhoto.src = photo;
+        photosList.appendChild(newPhoto);
+      });
+
+      offerDialogPanel.innerHTML = "";
+      offerDialogPanel.appendChild(newDialog);
+      
+
       offerDetailsDialog.style.display = 'block';
 
       onCardOpen(selectedOffer);
