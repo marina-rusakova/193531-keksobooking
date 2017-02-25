fbc45d98677f8b15d669f6983c818ae6b0940103
Marina Rusakova <marinarusakova111@gmail.com>
2017-02-25 00:59:08 -0800

    Проек подготовлен к допуску на финальную проверку 4

--- js/load.js
+++ js/load.js
@@ -8,26 +8,8 @@ window.load = (function () {
     xhr.open('GET', url);
 
     xhr.addEventListener('load', function (evt) {
-      if (evt.target.status >= 400) {
-        throw {
-          message: 'Failed to load data. Server returned status: ' + evt.target.status
-        }
-      } else if (evt.target.status >= 200) {
-        var result = JSON.parse(evt.target.response);
+      var result = JSON.parse(evt.target.response);
         onLoad(result);
-      }
-    });
-
-    xhr.addEventListener('error', function (e) {
-      throw {
-        message: 'Something\'s went wrong!'
-      }
-    });
-
-    xhr.addEventListener('timeout', function () {
-      throw {
-        message: 'Time\'s up!'
-      }
     });
 
     xhr.send();
